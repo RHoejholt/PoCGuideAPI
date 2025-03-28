acd940f7edc00e75237f1a95579cea6d1822f2a8
RHoejholt <158362000+RHoejholt@users.noreply.github.com>
2025-03-28 21:40:18 +0100

    claenaup

--- src/main/java/app/controllers/ChampionController.java
+++ src/main/java/app/controllers/ChampionController.java
@@ -13,19 +13,11 @@ import jakarta.persistence.EntityManagerFactory;
 
 
 public class ChampionController {
-    ArrayList<Champion> championsList = new ArrayList<>();
     private final EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
     ChampionDAO championDAO = ChampionDAO.getInstance(emf);
 
-    public ChampionController() {
-        Champion champion1 = new Champion(1, "Fiddlesticks", "scary guy");
-        Champion champion2 = new Champion(2, "Amumu", "sad guy");
-        championsList.add(champion1);
-        championsList.add(champion2);
-    }
-
     public void getAllChampions(Context ctx) {
-    ctx.json(championDAO.getChampions());
+        ctx.json(championDAO.getChampions());
     }
 
     public void getChampionById(Context ctx) {
@@ -34,8 +26,8 @@ public class ChampionController {
             ctx.json(match.get());
             return;
         } else {
-        ctx.status(404);
-        ctx.json(new ErrorMsg(404, "No content found for this request"));
+            ctx.status(404);
+            ctx.json(new ErrorMsg(404, "No content found for this request"));
         }
     }
 }
